0133ea5037cac81675fcbb639f6510a369f4d3ae
Debaswinimishra <debaswinimishra@gmail.com>
2024-04-16 12:47:19 +0530

    service file add

--- src/ClickEvent.spec.jsx
+++ src/ClickEvent.spec.jsx
@@ -1,8 +1,40 @@
 import { fireEvent, render, screen } from "@testing-library/react";
 
 import ClickEvent from "./ClickEvent";
+import cleandb from "./service";
 
-test("button click event testing", () => {
+// beforeAll(() => {
+//   console.log("*******beforeAll hook********");
+// });
+beforeEach(() => {
+  console.log("*******beforeEach hook********");
+  cleandb();
+});
+// afterAll(() => {
+//   console.log("*******afterAll hook********");
+// });
+// afterEach(() => {
+//   console.log("*******afterEach hook********");
+// });
+
+test("button click event testing 1", () => {
+  console.log("1");
+  render(<ClickEvent />);
+  let button = screen.getByRole("button");
+  fireEvent.click(button);
+  expect(screen.getByText("updated successfully")).toBeInTheDocument();
+});
+
+test("button click event testing 2", () => {
+  console.log("2");
+  render(<ClickEvent />);
+  let button = screen.getByRole("button");
+  fireEvent.click(button);
+  expect(screen.getByText("updated successfully")).toBeInTheDocument();
+});
+
+test("button click event testing 3", () => {
+  console.log("3");
   render(<ClickEvent />);
   let button = screen.getByRole("button");
   fireEvent.click(button);
